c1143760b08367dcd2bc50cadd165f920d7b933c
tamiradler <tamiradler@gmail.com>
2017-03-30 18:59:21 +0300

    Add files via upload

--- nav/nav-controller.js
+++ nav/nav-controller.js
@@ -1,9 +1,12 @@
+var NAV_MENUE_ITEMS_URL = "https://script.google.com/macros/s/AKfycbxWJgCP0uIg5AgVxORyPWbZw4p7IJotc1aIcIdNFFqjmzIpUog/exec?fun=getNavMenue";
+
 angular.module("navApp").controller('navCont',
-    function($scope) {
-        $scope.navList = getNavList();
+    function($scope,$http) {
+        $http.get(NAV_MENUE_ITEMS_URL)
+            .then(
+                function(response) {
+                    $scope.navList = response.data;
+                }
+            );
     }
 )
-
-function getNavList() {
-    return ['a','b','c','d','e'];
-}
